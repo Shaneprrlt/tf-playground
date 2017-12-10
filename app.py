60b7e3b8ad5f505979b29d46788d37c7a32734a4
Shane Perreault <shaneprrlt@gmail.com>
2017-12-10 13:06:05 -0600

    added linear regression/loss calc graphs, and a graph that actually trains a model using gradient descent to minimize the cost of a linear regression

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 import pdb
 
+# Graph1: Learning Basic TF Constructs
+
 # build a computational graph
 graph1 = tf.Graph()
 with graph1.as_default():
@@ -24,6 +26,8 @@ with graph1.as_default():
     # close the tf session
     session.close()
 
+# Graph2: Learning Basic TF Constructs
+
 graph2 = tf.Graph()
 with graph2.as_default():
     logs_path = 'logs/graph2'
@@ -37,11 +41,13 @@ with graph2.as_default():
         print(output)
     session.close()
 
+# Graph3: Running a Simple Linear Regression
+
 graph3 = tf.Graph()
 with graph3.as_default():
     logs_path = 'logs/graph3'
 
-    # linear model definition
+    # linear regression model definition
     W = tf.Variable([0.3], dtype=tf.float32)
     b = tf.Variable([-0.3], dtype=tf.float32)
     x = tf.placeholder(tf.float32)
@@ -53,7 +59,81 @@ with graph3.as_default():
         session.run(init)
 
         # run linear model
+        writer = tf.summary.FileWriter(logs_path, graph=session.graph)
         output = session.run(linear_model, feed_dict={ x: [1, 2, 3, 4] })
         print(output)
 
+    session.close()
+
+# Graph4: Running a Linear Regression and Calculate Loss (Cost Function)
+
+graph4 = tf.Graph()
+with graph4.as_default():
+    logs_path = 'logs/graph4'
+
+    # linear regression model definition
+    W = tf.Variable([0.3], dtype=tf.float32)
+    b = tf.Variable([-0.3], dtype=tf.float32)
+    input_data = tf.placeholder(tf.float32)
+    linear_model = W * input_data + b
+
+    # loss function
+    expected_output = tf.placeholder(tf.float32)
+    squared_deltas = tf.square(linear_model - expected_output)
+    loss_calc = tf.reduce_sum(squared_deltas)
+
+    with tf.Session() as session:
+        # initialize variables
+        init = tf.global_variables_initializer()
+        session.run(init)
+
+        # run linear model and loss function
+        writer = tf.summary.FileWriter(logs_path, graph=session.graph)
+        loss = session.run(loss_calc, feed_dict={ input_data: [1,2,3,4], expected_output: [0,-1,-2,-3] })
+        print(loss)
+
+    session.close()
+
+# Graph5: Minimize the Loss (Cost) by using Gradient Decent
+# to Correct Linear Regression Model
+
+graph5 = tf.Graph()
+with graph5.as_default():
+    logs_path = 'logs/graph5'
+
+    # linear regression model definition
+    W = tf.Variable([0.3], dtype=tf.float32)
+    b = tf.Variable([-0.3], dtype=tf.float32)
+    input_data = tf.placeholder(tf.float32)
+    linear_model = W * input_data + b
+
+    # loss function
+    expected_output = tf.placeholder(tf.float32)
+    squared_deltas = tf.square(linear_model - expected_output)
+    loss_calc = tf.reduce_sum(squared_deltas)
+
+    # optimizer (minimizes loss using gradient descent)
+    learning_rate = 0.01
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    train = optimizer.minimize(loss_calc)
+
+    with tf.Session() as session:
+        # initialize variables
+        init = tf.global_variables_initializer()
+        session.run(init)
+
+        writer = tf.summary.FileWriter(logs_path, graph=session.graph)
+
+        in_data = [1,2,3,4]
+        out_data = [0,-1,-2,-3]
+        total_epochs = 1000
+
+        # train the model
+        for i in range(total_epochs):
+            session.run(train, { input_data: in_data, expected_output: out_data })
+
+        # evaluate model accuracy
+        eval_W, eval_b, eval_loss = session.run([W, b, loss_calc], { input_data: in_data, expected_output: out_data })
+        print("W: %s, b: %s, loss: %s"%(eval_W, eval_b, eval_loss))
+    
     session.close()
